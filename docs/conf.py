0e36378d9f0c5d4aac7735708582fdb6c5882b70
Michael Mior <michael@onechronos.com>
2026-03-10 13:03:13 -0400

    feat: bump maturin and pyo3 to support Python 3.14

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@ sys.path.insert(0, os.path.abspath("../symbologyl2"))
 project = "symbologyl2"
 copyright = "2025, OneChronos Engineering"
 author = "OneChronos Engineering"
-release = "0.1.5"
+release = "0.1.6"
 
 extensions = [
     "sphinx.ext.autodoc",
